ba9cf06b782b47ca9b8dff061dde0155012f2a12
Yorichii1525 <149044030+Yorichii1525@users.noreply.github.com>
2025-04-07 15:46:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-btn = document.querySelector('button');
-btn.addEventListener('click',showMsg);
-function showMsg()
-{
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
     alert('Hello World!');
-}
+});
